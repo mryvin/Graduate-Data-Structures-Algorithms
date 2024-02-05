e44ca13d59a3854887feda9d9baefd717117f708
mryvin <89282726+mryvin@users.noreply.github.com>
2024-02-04 21:22:03 -0500

    Add files via upload

---
+++ fossil_conclusions-1.py
@@ -0,0 +1,13 @@
+BEFORE = 0
+OVERLAP = 1
+
+
+conclusions =  (  ( 0, 1, BEFORE  ),  ( 1, 2, BEFORE  ),  ( 2, 0, OVERLAP  )  ) 
+
+conclusions1 =  (  ( 0, 1, BEFORE  ),  ( 1, 2, BEFORE  ),  ( 2, 1, OVERLAP  )  ) 
+
+conclusions2 =  (  ( 0, 1, BEFORE  ),  ( 0, 2, BEFORE  ),  ( 2, 1, OVERLAP  )  ) 
+
+conclusions3 =  (  ( 0, 1, BEFORE  ),  ( 0, 2, BEFORE  ),  ( 2, 1, OVERLAP  ),  ( 3, 0, BEFORE  )  ) 
+
+conclusions4 =  (  ( 0, 1, BEFORE  ),  ( 0, 2, BEFORE  ),  ( 2, 1, OVERLAP  ),  ( 3, 0, BEFORE  ),  ( 2, 3, BEFORE  )  ) 
